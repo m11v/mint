f313c5d6c735f6c2784a27b93102be52e2f54051
irene.yu <irene.xin.yu@gmail.com>
2023-10-23 16:19:36 -0700

    Add AboutView

---
+++ lib/assets/images.dart
@@ -0,0 +1,4 @@
+class MintImages {
+  static const String mintminter =
+      'packages/mintminter_mint/assets/images/mintminter.png';
+}
--- lib/mint.dart
+++ lib/mint.dart
@@ -1,5 +1,6 @@
 library mintminter_mint;
 
 export 'ad/ad.dart';
+export 'assets/assets.dart';
 export 'utils/utils.dart';
 export 'views/views.dart';
