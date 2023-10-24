96a609f7e751e8923cdc47f454124b1e390142eb
Irene Yu <irene.xin.yu@gmail.com>
2023-10-24 10:01:45 -0700

    Add AppTile (#24)

--- lib/assets/images.dart
+++ lib/assets/images.dart
@@ -1,4 +1,10 @@
 class MintImages {
   static const String mintminter =
       'packages/mintminter_mint/assets/images/mintminter.png';
+
+  static const String huangdaxian =
+      'packages/mintminter_mint/assets/images/huangdaxian.png';
+
+  static const String gylq2023 =
+      'packages/mintminter_mint/assets/images/gylq2023.png';
 }
---
+++ lib/views/apps/apps.dart
@@ -0,0 +1,19 @@
+import 'package:mintminter_mint/mint.dart';
+
+export 'app_tile.dart';
+
+const huangdaxianTile = AppTile(
+  icon: MintImages.huangdaxian,
+  appName: '黄大仙灵签 - 求签解签',
+  appUrl:
+      'https://play.google.com/store/apps/details?id=com.mintminter.huangdaxian',
+  description: '有求必应，找黄大仙',
+);
+
+const guanyinlingqian2023Tile = AppTile(
+  icon: MintImages.gylq2023,
+  appName: '观音灵签 - 求签解签',
+  appUrl:
+      'https://play.google.com/store/apps/details?id=com.mintminter.guanyinlingqian2023',
+  description: '观音菩萨为众生消灾祈福解难',
+);
