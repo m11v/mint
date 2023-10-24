6ca8d44557d2b169218242fa9f74ccd6218af40d
Irene Yu <irene.xin.yu@gmail.com>
2023-10-24 14:34:58 -0700

    Add MintButton (#26)

---
+++ lib/views/mint_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class MintButton extends StatelessWidget {
+  const MintButton({
+    super.key,
+    required this.text,
+    this.onPressed,
+  });
+
+  final String text;
+  final VoidCallback? onPressed;
+
+  bool get isEnabled => onPressed != null;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: isEnabled
+              ? Theme.of(context).primaryColor
+              : Theme.of(context).disabledColor,
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+          ),
+        ),
+        onPressed: () {
+          onPressed?.call();
+        },
+        child: Text(
+          text,
+          style: TextStyle(
+              color: isEnabled ? Colors.white : Colors.white.withOpacity(0.7),
+              fontSize: 18,
+              fontWeight: FontWeight.bold),
+        ));
+  }
+}
--- lib/views/views.dart
+++ lib/views/views.dart
@@ -1,3 +1,4 @@
 export 'apps/apps.dart';
 export 'about_view.dart';
+export 'mint_button.dart';
 export 'page_content.dart';
