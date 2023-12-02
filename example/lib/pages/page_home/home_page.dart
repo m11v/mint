33b6b21ebb1583723eb1640a383aae7a14387d9e
irene.yu <irene.xin.yu@gmail.com>
2023-12-01 17:17:38 -0800

    fix ci

--- example/lib/pages/page_home/home_page.dart
+++ example/lib/pages/page_home/home_page.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mintminter_mint/han/mint_han_popup_menu_button.dart';
 import 'package:mintminter_mint/mint.dart';
-import 'package:mintminter_mint/views/mint_alert_dialog.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
